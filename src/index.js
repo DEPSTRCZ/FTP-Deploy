5a477e0e39f69dade3d82d5084d84b8313feb469
DEPSTRCZ <jiri.edelmann@gmail.com>
2023-06-14 10:38:48 +0200

    Added progress bar. Next cleanup

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 
 import { info, setFailed, getInput } from '@actions/core';
 import github from '@actions/github';
-import ftpDeployer from "ftp-deploy";
+import FtpDeployer from "ftp-deploy";
+import cliProgress from "cli-progress";
 
-const DeployFiles = new FtpDeployer();
+const Deployment = new FtpDeployer();
 
 info('Deploying...');
 
+async function generateProgressBar(progress) {
+    const completed = Math.round(50 * progress); // Number of completed characters
+    const remaining = 50 - completed; // Number of remaining characters
+  
+    const progressBar = '='.repeat(completed) + '>'.repeat(progress > 0 && progress < 1 ? 1 : 0) + ' '.repeat(remaining); // Generate the progress bar string
+    return progressBar;
 
+}
 try {
-    let Deployment = await DeployFiles.deploy({
+    let progress = 0
+    Deployment.deploy({
         sftp: JSON.parse(getInput('sftp')) || false,
         host: getInput('host', { required: true }),
         port: JSON.parse(getInput('port')) || 21,
@@ -17,30 +26,36 @@ try {
         password: getInput('password', { required: true }),
         remoteRoot: getInput('remote_folder') || './',
         localRoot: getInput('local_folder') || 'dist', // __dirname + '/local-folder',
-        deleteRemote: JSON.parse(getInput('cleanup')) || false, // If true, delete ALL existing files at destination before uploading
+        deleteRemote: JSON.parse(getInput('delete_remote')) || false, // If true, delete ALL existing files at destination before uploading
         include: JSON.parse(getInput('include')) || ['*', '**/*'], // this would upload everything except dot files
         exclude: JSON.parse(getInput('exclude')) || ['node_modules/**', 'node_modules/**/.*', '.git/**'], // e.g. exclude sourcemaps, and ALL files in node_modules (including dot files)
         forcePasv: JSON.parse(getInput('passive')) || true // Passive mode is forced (EPSV command is not sent)
     })
+
+    /*Deployment.on("uploading", function (data) {
+    //if (bar1.value === 0) bar1.start(data.totalFilesCount, 0);
+    bar1.update(data.transferredFileCount);
+    });*/
+    Deployment.on("uploaded", async function (data) {
+        console.log(data.totalFilesCount,data.transferredFileCount,data.filename,progress)
+        progress += 1 / data.totalFilesCount
+        const progressBar = await generateProgressBar(progress);
+        const procentage = Math.round((data.transferredFileCount / data.totalFilesCount) * 100)
+        console.log(`File: ${data.filename} [${progressBar}] ${procentage}%`)
+        if (data.transferredFileCount === data.totalFilesCount) {
+            console.log("S/FTP Upload has been done!")
+        }
+    });
+    Deployment.on("upload-error", function (data) {
+        console.log(data.err); // data will also include filename, relativePath, and other goodies
+    });
+    console.log("xd")
 } catch (error) {
     setFailed(error.message);
 }
 console.log("bef")
 
 
-deploy.on("uploading", function (data) {
-    console.log(data.totalFilesCount); // total file count being transferred
-    console.log(data.transferredFileCount); // number of files transferred
-    console.log(data.filename); // partial path with filename being uploaded
-});
-ftpDeploy.on("uploaded", function (data) {
-    console.log(data); // same data as uploading event
-});
-ftpDeploy.on("log", function (data) {
-    console.log(data); // same data as uploading event
-});
-ftpDeploy.on("upload-error", function (data) {
-    console.log(data.err); // data will also include filename, relativePath, and other goodies
-});
+
 
 console.log("aft")
